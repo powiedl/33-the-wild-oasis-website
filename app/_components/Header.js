1a8df0ee03e24441b6646ae4cf399d24a0d14055
powiedl <159603185+powiedl@users.noreply.github.com>
2024-08-11 18:41:26 +0200

    Header always on top (even if this is not so nice on the home page)

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,6 +3,7 @@ import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
+    //    <header className='border-b border-primary-900 px-8 py-5'>
     <header className='border-b border-primary-900 px-8 py-5 sticky top-0 z-10 bg-inherit'>
       <div className='flex justify-between items-center max-w-7xl mx-auto'>
         <Logo />
